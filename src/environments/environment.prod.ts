acebf444a066424f84ee5b9ca3e2d6ad237c130c
Chun Shen <mushrchun@gmail.com>
2018-01-05 00:26:24 +1100

    deploy to the firebase host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBnZxHgxM3FpQxLxAfuAqjWJPa72f8Rbvw',
+    authDomain: 'lightning-pod.firebaseapp.com',
+    databaseURL: 'https://lightning-pod.firebaseio.com',
+    projectId: 'lightning-pod',
+    storageBucket: 'lightning-pod.appspot.com',
+    messagingSenderId: '956260144377'
+  }
 };
