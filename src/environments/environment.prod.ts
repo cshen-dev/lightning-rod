fa52e017841b62dbdc87ed3ffad080fc0be30490
Chun Shen <mushrchun@gmail.com>
2018-01-28 13:54:57 +1100

    change firebase repo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBnZxHgxM3FpQxLxAfuAqjWJPa72f8Rbvw',
-    authDomain: 'lightning-pod.firebaseapp.com',
-    databaseURL: 'https://lightning-pod.firebaseio.com',
-    projectId: 'lightning-pod',
-    storageBucket: 'lightning-pod.appspot.com',
-    messagingSenderId: '956260144377'
+    apiKey: 'AIzaSyD0LndDRhNx91VniLGgPh1TLEEaeM_JsuQ',
+    authDomain: 'lightning-rod-3b2f6.firebaseapp.com',
+    databaseURL: 'https://lightning-rod-3b2f6.firebaseio.com',
+    projectId: 'lightning-rod-3b2f6',
+    storageBucket: 'lightning-rod-3b2f6.appspot.com',
+    messagingSenderId: '959494144243'
   }
 };
