14d6afb0a0be935b815d6fc1738de038604e9a58
Chun Shen <mushrchun@gmail.com>
2018-03-06 17:33:33 +1100

    add google analytic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    window['ga']('create', 'UA-115181807-1', 'auto');
+
+    this.router.events.subscribe( event => {
+      if (event instanceof NavigationEnd) {
+        window['ga']('send', 'pageview', this.router.url);
+      }
+    });
+  }
 }
